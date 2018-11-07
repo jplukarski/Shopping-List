776bb8d343faf2ea3315ea206cb27dc03f9718ce
JPlukarski <jwplukarski@gmail.com>
2018-11-07 12:05:14 -0600

    fixed server.js error

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const bodyParser = require("body-parser");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-
 // Bodyparser Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -12,8 +11,6 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 }
-// Add routes, both API and view
-app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/shoppingList");
