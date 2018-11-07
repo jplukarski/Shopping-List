5e46a561574aceedff16dcde213abef4ea7889b9
JPlukarski <jwplukarski@gmail.com>
2018-11-07 12:40:57 -0600

    added delete route

--- routes/api/items.js
+++ routes/api/items.js
@@ -4,15 +4,23 @@ const router = express.Router();
 // Item Model
 const Item = require("../../models/Item");
 
-// @route   Post api/items
-// @desc    Create a post
+// @route   POST api/items
+// @desc    Create a item
 // @access  Public
 router.post("/", (req, res) => {
     const newItem = new Item({
         name: req.body.name
     });
-
     newItem.save().then(item => res.json(item));
+});
+
+// @route   DELETE api/items
+// @desc    Delete a item
+// @access  Public
+router.delete("/:id", (req, res) => {
+    Item.findById(req.params.id)
+        .then(item => item.remove().then(() => res.json({ success: true })))
+        .catch(err => res.status(404).json({ success: false }))
 });
 
 module.exports = router;
