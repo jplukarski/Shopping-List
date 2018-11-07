c382e92466b53590465c37beb9a3a8029d1c13c9
JPlukarski <jwplukarski@gmail.com>
2018-11-07 12:17:15 -0600

    set up routes

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const app = express();
 const PORT = process.env.PORT || 3001;
+const items = require("./routes/api/items");
+
+
 
 // Bodyparser Middleware
 app.use(express.urlencoded({ extended: true }));
@@ -14,6 +17,9 @@ if (process.env.NODE_ENV === "production") {
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/shoppingList");
+
+// Use Routes
+app.use("/api/items", items);
 
 // Start the API server
 app.listen(PORT, function () {
